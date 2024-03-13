3b60b0ab09865ca835788046a1f876a539ed8baa
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-03-13 11:50:32 -0300

    6747

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i <botoes.length; j++){
+for (let i = 0; i < botoes.length; j++) {
     botoes[i].onclick = function () {
 
-for (let i = 0; i <botoes.length; j++){
-    botoes[j].classList.remove("ativo");
-    textos[j].classList.remove("ativo");
+        for (let i = 0; i < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
-botoes[i].classList.add("ativo");
-textos[i].classList.add("ativo");
-}
-}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date ("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2024-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2024-12-01T00:00:00");
+
+const tempos = [tempoObjetivo1, empoObjetivo2, empoObjetivo3, empoObjetivo4];
