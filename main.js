b1648a3395f0132341a7c71e451f5a6ddcd88035
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-03-20 11:12:15 -0300

    127441

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ const textos = querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length; j++) {
     botoes[i].onclick = function () {
 
-        for (let i = 0; i < botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
-            textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo");
-        textos[i].classList.add("ativo");
     }
 }
 const contadores = document.querySelectorAll(".contador");
