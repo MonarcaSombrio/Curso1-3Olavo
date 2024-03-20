3e70eb132d40c16cdc5febbcd46d4b3c90386771
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-03-20 11:40:04 -0300

    7

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-const botoes = document.querySelectorAll(".botao");
-const textos = querySelectorAll(".aba-conteudo");
+const botoes = document.querySelectorAll(".botao");//variável que armazena todos os itens com a classe botão
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantidade de botoes
+
+    botoes[i].onclick = function () {//cria uma função 
+
+        for (let j = 0; j < botoes.length; j++) { // loop que executa enquanto j<quantidade de botoes
+            botoes[j].classList.remove("ativo");  //remove a palavra ativo da classe.
         }
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
+    console.log();
 }
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date ("2024-12-05T00:00:00");
-const tempoObjetivo3 = new Date ("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date ("2024-12-01T00:00:00");
-
-const tempos = [tempoObjetivo1, empoObjetivo2, empoObjetivo3, empoObjetivo4];
