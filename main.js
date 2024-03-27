b68dbc34622a4d8c48b6425003a09dcdfc7315c1
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-03-27 11:50:04 -0300

    6845

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");//variável que armazena todos os itens com a classe botão
-
+const textos = documento.querySelectorAll (".aba-conteudo");//Variavel que armazena todos os itens com a aba-conteudo
 
 for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantidade de botoes
 
@@ -9,6 +9,7 @@ for (let i = 0; i < botoes.length; i++) {// loop que executa enquanto i<quantida
             botoes[j].classList.remove("ativo");  //remove a palavra ativo da classe.
         }
         botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
+        textos[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
     console.log();
 }
