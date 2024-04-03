363b5ef86268315a14bf220fed4bf703285be19e
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-04-03 11:32:10 -0300

    3952

--- main.js
+++ main.js
@@ -13,12 +13,16 @@ for (let i = 0; 1 < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 
+}
 
-    const contadores = document.querySelectorAll(".contador");
+const contadores = document.querySelectorAll(".contador");
 
-    const tempoObjetivo1 = new date("2024-10-05T00:00:00");
+const tempoObjetivo1 = new date("2024-10-05T00:00:00");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new date();
 
-    contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 }
