3c3633e17d1534e4463d7168c26a3d43a752ca0a
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-04-10 11:51:39 -0300

    jhj

--- main.js
+++ main.js
@@ -18,8 +18,14 @@ for (let i = 0; 1 < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 
 const tempoObjetivo1 = new date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new date("2024-10-05T00:00:00");
+const tempoObjetivo4 = new date("2024-10-05T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo1) {
 
