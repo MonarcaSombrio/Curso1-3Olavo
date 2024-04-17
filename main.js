70296b739b668ff988e08e14959e5f2aa4ec48a9
MonarcaSombrio <106557119+MonarcaSombrio@users.noreply.github.com>
2024-04-17 11:04:45 -0300

    987

--- main.js
+++ main.js
@@ -23,6 +23,14 @@ const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i] textContent = calculaTempo(tempos[i]);
+    }
+}
+
+setInterval(atualizaCronometro, 1000);
+
 for (let x = 0; x < contadores.length; x++) {
     contadores[x].textContent = calculaTempo(tempos[x]);
 }
